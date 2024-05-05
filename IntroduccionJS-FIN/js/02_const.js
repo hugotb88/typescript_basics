1e135fd73af85bd20027016302bd58feed9b54ee
Victor Hugo Olvera Cruz <hugotb88@gmail.com>
2024-05-05 17:54:28 -0600

    CONST concepts

---
+++ IntroduccionJS-FIN/js/02_const.js
@@ -0,0 +1,13 @@
+/*
+const is used for constants
+No changes after initialization
+Const should have a initial value
+*/
+
+
+const client = "Victor"
+client = 30 //not valid
+console.log(client)
+
+const price = 1000
+console.log(price)
