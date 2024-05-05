bfba1dbfb0851ea02afe1e3472e154d8d4ab500a
Victor Hugo Olvera Cruz <hugotb88@gmail.com>
2024-05-05 17:49:55 -0600

    VAR and LET concepts

---
+++ IntroduccionJS-FIN/js/01_variables_basics.js
@@ -0,0 +1,15 @@
+/*
+ * var, let and const
+ * var is not used anymore
+ * don't start variable names with numbers, * or special characters like "let 99name"
+ * */
+
+//let value could  be re-assigned
+let client = "Victor"
+client = 30
+console.log(client)
+
+//let variables could be initialized without default value
+let price
+price = 1000
+console.log(price)
